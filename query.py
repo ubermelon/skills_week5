74a5926730f621c8daa2e47947a3d2cb5dc95991
ubermelon <ubermelonengineer@gmail.com>
2016-05-08 19:34:02 -0700

    attempted part 2 of SQLAlchemy skills assessment

--- query.py
+++ query.py
@@ -18,24 +18,31 @@ init_app()
 # -------------------------------------------------------------------
 # Start here.
 
-
 # Part 2: Write queries
 
 # Get the brand with the **id** of 8.
+Brand.query.get(8)
 
 # Get all models with the **name** Corvette and the **brand_name** Chevrolet.
+Model.query.filter(Model.name == 'Corvette', Model.brand_name == 'Chevrolet').all()
 
 # Get all models that are older than 1960.
+Model.query.filter(Model.year > 1960).all()
 
 # Get all brands that were founded after 1920.
+Brand.query.filter(Brand.founded > 1920).all()
 
 # Get all models with names that begin with "Cor".
+Model.query.filter(Model.name.like('Cor%')).all()
 
 # Get all brands with that were founded in 1903 and that are not yet discontinued.
+Brand.query.filter(Brand.founded == 1903, Brand.discontinued.isnot(None)).all()
 
 # Get all brands with that are either discontinued or founded before 1950.
+Brand.query.filter((Brand.founded < 1950) | (Brand.discontinued == None)).all()
 
 # Get any model whose brand_name is not Chevrolet.
+Model.query.filter(db.not_(Model.brand_name == 'Chevrolet')).all()
 
 # Fill in the following functions. (See directions for more info.)
 
@@ -43,13 +50,19 @@ def get_model_info(year):
     '''Takes in a year, and prints out each model, brand_name, and brand
     headquarters for that year using only ONE database query.'''
 
-    pass
+	q = db.session.query(Model.brand_name, Model.name, Model.year, Brand.headquarters).join(Brand)
+
+    for year in q:
+    	print q.brand_name, q.name, q.year, q.headquarters
 
 def get_brands_summary():
     '''Prints out each brand name, and each model name for that brand
      using only ONE database query.'''
 
-    pass
+    brand = db.session.query(Brand.name, Model.brand_name).join(Model)
+
+    for b_name, m_name in brand:
+    	print b_name.name, m_name.name
 
 # -------------------------------------------------------------------
 
